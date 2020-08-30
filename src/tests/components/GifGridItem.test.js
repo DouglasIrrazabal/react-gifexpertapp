f68efd38362ce9f1615f1b7994d1816b8afa6a1d
Douglas <douglasirrazaba@gmail.com>
2020-08-30 12:09:16 -0400

    GifGridItem.test.js added

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGridItem } from '../../components/GifGridItem';
+
+describe('Pruebas en GifGridItem', () => {
+
+    const title = 'Hello World';
+    const url = 'https://www.google.com';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />)
+
+    test('debe de mortrar el componente correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    test('debe tener un parrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+    
+    test('debe tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+
+    test('debe de tener animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        // console.log(div.prop('className'));
+        expect(div.prop('className').includes('animate__fadeIn')).toBe(true);
+    })
+    
+    
+})
